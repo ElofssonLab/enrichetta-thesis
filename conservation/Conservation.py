c09b51725aa74fc39a70de2b1eb26ab139bb5f58
kettina <mileti.enrichetta@gmail.com>
2014-03-20 08:49:48 +0100

    Add Conservation.py

--- conservation/Conservation.py
+++ conservation/Conservation.py
@@ -9,116 +9,109 @@ from Bio.Seq import Seq
 from Bio.Alphabet.IUPAC import protein
 from Bio import AlignIO
 from scipy import stats
-path='/media/data/hhblits_files/a2m/'
-#path='/home/kettina/Scrivania/Nuova/'
-path=sorted(glob.glob(path+'*.a2m'))
+#path='/media/kettina/D874-68C8/alignment/fasta/'
+path='/media/kettina/D874-68C8/alignment/prova/'
+#path='/home/enrichetta/Desktop/alignment/fasta/'
+#path='/media/data/fasta/'
+path=sorted(glob.glob(path+'*.sth'))
 
-dis_annotation={}
-ord_annotation={}
-n_dis_region={}
-n_ord_region={}
-av={}
-
+def variable():
+	global dis_annotation, ord_annotation, n_dis_region, n_ord_region, av, disar, ordar, hom_used, hom_original, leno, lend
+	dis_annotation={}
+	ord_annotation={}
+	n_dis_region={}
+	n_ord_region={}
+	av={}
+	disar={}
+	ordar={}
+	hom_used={}
+	hom_original={}
+	leno={}
+	lend={}
 
 def entropy_matrix(files):
+	
 	'''compute the entropy matrix for each sequence starting from multiple sequence alignment'''
-	global name
+	global name,tot
+	global pos
+	global mat
+	pos=[]
 	drive, path=os.path.splitdrive(files)
      	path, filename=os.path.split(path)
      	name=filename.split('.')[0]
-	global hom_used
-	hom_used={}
-	global hom_original
-	hom_original={}
-	global mat
-	align= AlignIO.read(files, 'fasta')
+	align= AlignIO.read(files, 'stockholm')
+	tot=len(align)
 	hom_original[name]=len(align)
 	target=align[0].seq
 	alphabet=[]
+	alphabet_gap=[]
 	pr =set(protein.letters)
 	for el in pr:
      		alphabet.append(el)
 
-	alphabet.append('-')
+	alphabet_gap.append('-')
+	#alphabet_gap.append('.') # use this if the format of the alignment is a2m
 	gap=0
-
-	for element in target:
-    		gap+=element.count('.')
-    		gap+= element.count('-')
-	thr=(len(target)-gap)*80/100
-
+	
+	#for element in target:
+    		#gap+= element.count('-')
+	#thr=(len(target)-gap)*80/100
+	
+	for i,el in enumerate(target):
+		if el=='-':
+			pos.append(i)
+	
 	al=[]
 	hom_n=0
 	for element in align[0:]:
-     		gaps=0
-     		ins=0
-     		ins+=element.seq.count('.')
-     		gaps+=element.seq.count('-')
-     		inner_thr=len(element.seq)-gaps
-     		if inner_thr >=thr:
-         		el=element.seq
-         		al.append(el)
-			hom_n+=1
+     		#gaps=0
+     		#ins=0
+     		#ins+=element.seq.count('.')
+     		#gaps+=element.seq.count('-')
+     		#inner_thr=len(element.seq)-gaps
+     		#if inner_thr >=thr:
+         	el=element.seq
+         	al.append(el)
+		hom_n+=1
 	hom_used[name]=hom_n
+	
 	mat= np.zeros(shape=((len(target)),len(alphabet)))
-
+	mat_gap= np.zeros(shape=((len(target)),len(alphabet_gap)))
 	for i in al:
-
 		for x, y in enumerate (i):
-        
-
-
 			if y in alphabet:
 				n=alphabet.index(y)
              			mat[x][n]+=1
-            
-         		else:
+				            
+         		if y in alphabet_gap:
+				n=alphabet_gap.index(y)
+				mat_gap[x][n]+=1
+			else:
              			pass
-
-
-	sumline=np.nansum(mat, axis=1)
+			
 	for v in range(len(target)):
-		mat[v]/=sumline[v]
-
-
+		mat[v]/=tot
+		mat_gap[v]/=tot
+	
 	for v in range(len(target)):
-		mat[v]=mat[v]*np.log2(mat[v])
-
+		g=mat_gap[v][0]
+		ind=1-(g)
+		mat[v]=(mat[v]*np.log2(mat[v]))/(ind)
 		mat[v]=np.absolute(mat[v])
 		mat[v]=np.round(mat[v],decimals=5)
 
 
-
-	#df=pd.DataFrame(mat, index=[range(len(target))],columns=[alphabet])
-	#pd.set_option('display.height',20)
-	#pd.set_option('display.max_rows', len(target))
-	#pd.set_option('display.max_columns',len(alphabet))
-	#pd.set_option('display.precision',3)
-	#print df
-
-
-
 def entropy():
 	#return a list with the entropies of each sequence
-	sum_h=np.nansum(mat,axis=1)
-	global entrop
+	global entrop	
+	sum_h=np.nansum(mat,axis=1)		
 	entrop=[]
 	for x,y in zip(range(len(sum_h)),sum_h):
-	
 		entrop.append(round(y,2))
-	
-	dis_reg=[]
-	ord_reg=[]
-
-
-	for i,j in zip(entrop, range(len(entrop))):
-		if math.isnan(float(entrop[j])):
-	
-			 entrop[j]='#'
-		
-	
-	entrop=[x for x in entrop if x!='#']
-	
+	offset=0
+	for el in pos:
+		del entrop[el-offset]
+		offset+=1
 	
 
 def regions(dis_fasta):
@@ -134,102 +127,126 @@ def regions(dis_fasta):
                     line_str=str(line.seq)
                     dis_regions[line.id]= [(a.start(),a.end()) for a in list(re.finditer('0+',line_str))]
                     ord_regions[line.id]= [(a.start(),a.end()) for a in list(re.finditer('1+',line_str))]
-        
-	#print dis_regions
-	#print ord_regions
 	    
 def disorder_entropy():
-	global dis_ar,ln_d,len_d
 	len_d=[]
 	diss=[]
-	for element in sorted(dis_regions):
-		
+	for element in sorted(dis_regions):	
 		n_dis_region[element]=len(dis_regions[element])
 		if element==name:
-			
 			if  len(dis_regions[element])==0:
-				dis_annotation[name]='*'
-				dis_ar=np.array(diss, dtype=np.float64)
+				disar[element]=[]
 				ln_d=  None
 				len_d.append(ln_d)
+				lend[element]=len_d		
 			else:
-			
-			
-				for coor in dis_regions[name]:
-					
+				for coor in dis_regions[name]:	
 					if coor!=' ':
 						x=int(coor[0])
 						y=int(coor[1])
-						
 						ln_d=y-x
-						
-							
-						len_d.append(ln_d)
-							
-						s=0
-						n=0
-						for i in entrop[x:y+1]: 
-							n+=1
-							s+=i
-						s=s/n
-						
-						diss.append(round(s,2)) 
-						dis_ar=np.array(diss, dtype=np.float64)
-	
-						l=sum(diss)
-						x= len(diss)
-						dis_annotation[name]=round((l/x),2)
+						if ln_d>=30:
+							len_d.append(ln_d)
+							s=0
+							n=0
+							for i in entrop[x:y+1]: 
+								n+=1
+								s+=i
+							s=s/n
+							diss.append(round(s,2)) 
 					else:
 						pass
+		
+				lend[element]=len_d
+				disar[element]=diss
 	
-
-def order_entropy():
-	global ord_ar,ln_o,len_o				
+			
+def order_entropy():	
 	ordrr=[]
 	len_o=[]
 	for element in sorted(ord_regions):
 		n_ord_region[element]=len(ord_regions[element])
 		if element==name:
-			
 			if  len(ord_regions[element])==0:
-				ord_annotation[element]='*'
-				ord_ar=np.array(ordrr, dtype=np.float64)
 				ln_o= None
 				len_o.append(ln_o)
+				leno[name]=len_o
+				ordar[element]=ordrr
 			else:
-				
-				for coor in ord_regions[element]:
-					
-					
+				for coor in ord_regions[element]:	
 					if coor!=' ':
-				
-						
 						x=int(coor[0])
 						y=int(coor[1])
-					
 						ln_o=y-x
-							
-						len_o.append(ln_o)
-							
-						s=0
-						n=0
-						for i in entrop[x:y+1]: 
-							n+=1
-							s+=i
-						s=s/n
-						
-						ordrr.append(round(s,2)) 
-						ord_ar=np.array(ordrr, dtype=np.float64)
-						
-						l=sum(ordrr)
-						x= len(ordrr)
-						ord_annotation[name]=round((l/x),2)
-				
+						if ln_o >=30:			
+							len_o.append(ln_o)
+							s=0
+							n=0
+							for i in entrop[x:y+1]: 
+								n+=1
+								s+=i
+							s=s/n
+							ordrr.append(round(s,2)) 
 					else:
 						pass
+
+				leno[element]=len_o
+				ordar[element]=ordrr
 	
+def normalize_result():
+	global minmax
+	minmaxdis=[]
+	minmaxord=[]
+	for k,v in sorted(ordar.iteritems()):
+		if len(v)!=0:
+			for el in v:
+				minmaxord.append(el)
+		else:
+			pass
 	
+	for k,v in sorted(disar.iteritems()):
+		if len(v)!=0:
+			for el in v:
+				minmaxdis.append(el)
+		else:
+			pass
+			
+	mdis=min(minmaxdis)
+	Mdis=max(minmaxdis)
+	mord=min(minmaxord)
+	Mord=max(minmaxord)
 	
+	for k,v in sorted(ordar.iteritems()):
+		new=[]
+		if len(v)!=0:
+			for el in v:
+				el=round((el-mord)*4.3/float(Mord),2)
+				new.append(el)
+		ordar[k]=new
+	
+	for k,v in sorted(disar.iteritems()):
+		new=[]
+		if len(v)!=0:
+			for el in v:
+				el=round((el-mdis)*4.3/float(Mdis),2)
+				new.append(el)
+		disar[k]=new
+	
+def average_ord():
+	for k,v in sorted(ordar.iteritems()):
+		if len(v)==0:
+			ord_annotation[k]='*'
+		else:
+			ord_annotation[k]=round(sum(v)/len(v),2)
+	
+def average_dis():
+	for k,v in sorted(disar.iteritems()):
+		if len(v)==0:
+			dis_annotation[k]='*'
+		else:
+			dis_annotation[k]=round(sum(v)/len(v),2)
+	
+'''	
 def t_test():
 	
 	global t_val, p_val
@@ -253,7 +270,7 @@ def t_test():
 		else:
 			p_val[name]=round(p,2)
 
-
+'''
 def results():
 	for k,j in zip(dis_annotation, ord_annotation):
 		if dis_annotation[k]!='*' and ord_annotation[j]!='*':
@@ -270,27 +287,50 @@ def results():
 			av[k]=ord_annotation[j]
 		elif dis_annotation[k]!='*' and ord_annotation[j]=='*':
 			av[k]=dis_annotation[k]
-					 
+
+
+def print_result1():
+	with open('result.txt','w') as p:
+		saveout=sys.stdout
+		sys.stdout=p
+		print '{0:^12} {1:^15} {2:^12} {3:^12} {4:^10}{5:^15}{6:^10}'.format('ID','n_dis_region','n_ord_region','n_hom_used','<Hd>','<Ho>','av_entropy')
+		for l in sorted(dis_annotation):
+			try:
+			
+				print '{0:^12} {1:^15} {2:^12} {3:^12}{4:^12}{5:^12}{6:^12}'.format(l,n_dis_region[l],n_ord_region[l],hom_used[l],dis_annotation[l],ord_annotation[l],av[l])
+			except:
+				print '{0:^12} {1:^15} {2:^15} {3:^12}{4:^10}{5:^12}{6:^12}'.format(name ,'*', '*', '*', '*', '*', '*')
+
+		p.close()
+		sys.stdout=saveout
+
+def print_result2():
+	with open('resultwithlen.txt','w') as p:
+		saveout=sys.stdout
+		sys.stdout=p
+		print '{0:^12} {1:^15} {2:^15} {3:^12}{4:^12}{5:^12}{6:^12}{7:^12}'.format('ID','len_dis_reg','dis_H', 'len_ord_reg', 'ord_H','<Hd>','<Ho>','av_entropy')
+		for k,v in sorted(leno.iteritems()):
+			if k in ordar and lend and disar and dis_annotation and ord_annotation and av:
+				for el in map(None,lend[k], disar[k],v, ordar[k]):
+					try:
+						print '{0:^12} {1:^15} {2:^15} {3:^12}{4:^12}{5:^12}{6:^12}{7:^12}'.format( k,el[0],el[1], el[2], el[3], dis_annotation[k], ord_annotation[k], av[k])
+					except:
+						print '{0:^12} {1:^15} {2:^15} {3:^12}{4:^12}{5:^12}{6:^12}{7:^12}'.format(name ,'*', '*', '*', '*', '*', '*', '*')
+		p.close()
+		sys.stdout=saveout
 	
-	for el in map(None,len_d,dis_ar,len_o,ord_ar):
-		print '{0:^12} {1:^15} {2:^15} {3:^12}{4:^12}{5:^12}{6:^12}{7:^12}'.format(name ,el[0], el[1], el[2], el[3], dis_annotation[name], ord_annotation[name], av[name])
-
-	#print '{0:^12} {1:^15} {2:^15} {3:^12}{4:^12}'.format(name,dis_annotation[name],ord_annotation[name], t_val[name], p_val[name])
-	#print '{0:^12} {1:^15} {2:^15} {3:^12}{4:^12}{5:^12}{6:^12}{7:^12}'.format(name,n_dis_region[name],n_ord_region[name],hom_original[name],hom_used[name],dis_annotation[name],ord_annotation[name],av[name])
-
-print '{0:^12} {1:^15} {2:^15} {3:^12}{4:^12}{5:^12}{6:^12}{7:^12}'.format('ID','len_dis_reg','dis_H', 'len_ord_reg', 'ord_H','<Hd>','<Ho>','av_entropy')
-#print '{0:^12} {1:^15} {2:^15} {3:^12} {4:^12}{5:^12}{6:^12}{7:^12}'.format('ID','n_dis_region','n_ord_region','n_hom_orig','n_hom_used','<Hd>','<Ho>','av_entropy')
-#print '{0:^12} {1:^15} {2:^15} {3:^12}{4:^12}'.format('ID','<Hd>','<Ho>', 't-value', 'p-value')
-
-regions('/media/data/dataset_oxana/disorder_annotation.fasta')
-#regions('/home/kettina/Scrivania/disorder_annotation.fasta')
-
-for files in path:
-	
-	entropy_matrix(files)
-	entropy()
-	disorder_entropy()
-	order_entropy()
-	t_test()
+if __name__ == '__main__':
+#	regions('/media/data/dataset_oxana/disorder_annotation.fasta')
+	regions('/home/kettina/Scrivania/disorder_annotation.fasta')
+	variable()
+	for files in path:
+		entropy_matrix(files)
+		entropy()
+		disorder_entropy()
+		order_entropy()
+	normalize_result()
+	average_ord()
+	average_dis()
 	results()
-
+	print_result1()
+	print_result2()
